83f53fa8d28a1ea04cb3f500a898c10b9c06b2f8
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-03 13:47:17 +0000

    Webservice to save user profile added
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@4 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/controllers/bartsy/UserController.groovy
+++ grails-app/controllers/bartsy/UserController.groovy
@@ -2,14 +2,40 @@ package bartsy
 
 import grails.converters.JSON
 
+
 class UserController {
 
-    def index() { }
-    
-    def saveUserProfile = {
-        //def json = JSON.parse(params)
-        BartsyUserDetails bartsyUserDetails = new BartsyUserDetails()
-        def maxId = BartsyUserDetails.createCriteria().get { projections { max "id" }}
-        println maxId 
-    }
+	def index() {
+	}
+
+	def saveUserProfile = {
+		def json = JSON.parse(request)
+		Map response = new HashMap()
+		def userProfile = UserProfile.findByUserName(json.userName)
+		if(userProfile) {
+			response.put("bartsyUserId",userProfile.barstyId)
+			render(text:response as JSON ,  contentType:"application/json")
+		}
+		else{
+			UserProfile userProfileToSave = new UserProfile()
+			userProfileToSave.setUserName(json.userName)
+			userProfileToSave.setName(json.name)
+			userProfileToSave.setLoginId(json.loginId)
+			userProfileToSave.setLoginType(json.loginType)
+			userProfileToSave.setGender(json.gender)
+			def maxId = UserProfile.createCriteria().get {
+				projections { max "barstyId" }
+			} as Long
+			if(maxId){
+				maxId = maxId+1
+			}
+			else{
+				maxId = 100001
+			}
+			userProfileToSave.setBarstyId(maxId)
+			response.put("bartsyUserId",maxId)
+			userProfileToSave.save()
+			render(text:response as JSON ,  contentType:"application/json")
+		}
+	}
 }
