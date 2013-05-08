993ea7c28efac3dc07356c201292118677968161
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-08 14:59:08 +0000

    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@13 c3b44e55-a100-42f6-9c86-32dbf579054b

---
+++ grails-app/services/bartsy/AndroidPNService.groovy
@@ -0,0 +1,60 @@
+package bartsy
+
+import com.google.android.gcm.server.Message
+import com.google.android.gcm.server.Result
+import com.google.android.gcm.server.Sender
+
+class AndroidPNService {
+
+    public void sendPN(String orderStatus,String orderId,String registrationId){
+        try {
+            log.info("Came into the android push")
+			println "Came into the android push"
+            //String apiKey = "AIzaSyD8HRgmmsbrAqBOFQGngf5MkHYG1qacj9Q" // development
+            String apiKey = "AIzaSyBXDQMxqmGHJ7fOQeysFDFmrZRpuI1vp40" // production
+            Sender sender = new Sender(apiKey)
+            Message message
+            message = new Message.Builder()
+//            .collapseKey("1")
+//            .timeToLive(3)
+//            .delayWhileIdle(true)
+            .addData("orderStatus",orderStatus)
+            .addData("orderId",orderId)
+            .addData("badgeCount",String.valueOf(1))
+            .build();
+            Result result = sender.send(message,registrationId,1);
+            log.info("Result---------->>>"+result.toString())
+        } catch (Exception e) {
+            log.info("Android PN Exception"+e.getMessage())
+        }
+    }
+	
+	public void sendPlaceOrderPN(String orderStatus,String orderId,String itemName,String orderTime,String basePrice,String tipPercentage,String totalPrice,String registrationId){
+		try {
+			log.info("Came into the android push")
+			//println "Came into the android push"
+			//String apiKey = "AIzaSyD8HRgmmsbrAqBOFQGngf5MkHYG1qacj9Q" // development
+			String apiKey = "AIzaSyBXDQMxqmGHJ7fOQeysFDFmrZRpuI1vp40" // production
+			Sender sender = new Sender(apiKey)
+			Message message
+			message = new Message.Builder()
+//            .collapseKey("1")
+//            .timeToLive(3)
+//            .delayWhileIdle(true)
+			.addData("orderStatus",orderStatus)
+			.addData("orderId",orderId)
+			.addData("itemName",itemName)
+			.addData("orderTime",orderTime)
+			.addData("basePrice",basePrice)
+			.addData("tipPercentage",tipPercentage)
+			.addData("totalPrice",totalPrice)
+			.addData("badgeCount",String.valueOf(1))
+			.build();
+			Result result = sender.send(message,registrationId,1);
+			log.info("Result---------->>>"+result.toString())
+		} catch (Exception e) {
+			log.info("Android PN Exception"+e.getMessage())
+		}
+	}
+
+}
