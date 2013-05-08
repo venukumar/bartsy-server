2579766c70db6e3ab99348071443ce2003608cb7
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-08 14:58:35 +0000

    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@11 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/controllers/bartsy/UserController.groovy
+++ grails-app/controllers/bartsy/UserController.groovy
@@ -7,14 +7,19 @@ class UserController {
 
 	def index() {
 	}
-
+	
+	/**
+	 * Author 		: Swetha Bhatnagar
+	 * Description	: This is a webservice to be called while user regsitration to store user profile data into the server		
+	 */
 	def saveUserProfile = {
 		def json = JSON.parse(request)
 		Map response = new HashMap()
 		def userProfile = UserProfile.findByUserName(json.userName)
 		if(userProfile) {
-			response.put("bartsyUserId",userProfile.barstyId)
-			render(text:response as JSON ,  contentType:"application/json")
+			response.put("bartsyUserId",userProfile.bartsyId)
+			response.put("errorCode","1")
+			response.put("errorMessage","UserId already exists")
 		}
 		else{
 			UserProfile userProfileToSave = new UserProfile()
@@ -23,19 +28,63 @@ class UserController {
 			userProfileToSave.setLoginId(json.loginId)
 			userProfileToSave.setLoginType(json.loginType)
 			userProfileToSave.setGender(json.gender)
-			def maxId = UserProfile.createCriteria().get {
-				projections { max "barstyId" }
-			} as Long
+			userProfileToSave.setDeviceToken(json.deviceToken)
+			userProfileToSave.setDeviceType(json.deviceType as int)
+			def maxId = UserProfile.createCriteria().get { projections { max "bartsyId"
+				} } as Long
 			if(maxId){
 				maxId = maxId+1
 			}
 			else{
 				maxId = 100001
 			}
-			userProfileToSave.setBarstyId(maxId)
-			response.put("bartsyUserId",maxId)
-			userProfileToSave.save()
-			render(text:response as JSON ,  contentType:"application/json")
+			userProfileToSave.setBartsyId(maxId)
+			if(userProfileToSave.save()){
+				response.put("bartsyUserId",maxId)
+				response.put("errorCode","0")
+				response.put("errorMessage","Save Successful")
+			}
+			else{
+				response.put("errorCode","1")
+				response.put("errorMessage","Save not successful")
+			}
 		}
+		render(text:response as JSON ,  contentType:"application/json")
+	}
+	
+	
+	/**
+	 * Author 		: Swetha Bhatnagar
+	 * Description	: This is a webservice to be called while user checks in to store user status into the server
+	 */
+	def userCheckIn = {
+		def json = JSON.parse(request)
+		def userProfile = UserProfile.findByBartsyId(json.bartsyId)
+		def venue = Venue.findByVenueId(json.venueId)
+		def response = [:]
+		if(userProfile && venue){
+			if(CheckedInUsers.findByUserProfileAndVenue(userProfile,venue)){
+				response.put("errorCode","1")
+				response.put("errorMessage","User already Checked In")
+			}
+			else{
+				def checkedInUsers = new CheckedInUsers()
+				checkedInUsers.setUserProfile(userProfile)
+				checkedInUsers.setVenue(venue)
+				if(checkedInUsers.save(flush:true)){
+					response.put("errorCode","0")
+					response.put("errorMessage","User Checked In Successfully")
+				}
+				else{
+					response.put("errorCode","1")
+					response.put("errorMessage","User check in failed")
+				}
+			}
+		}
+		else{
+			response.put("errorCode","1")
+			response.put("errorMessage","User ID or Venue ID does not exist")
+		}
+		render(text:response as JSON ,  contentType:"application/json")
 	}
 }
