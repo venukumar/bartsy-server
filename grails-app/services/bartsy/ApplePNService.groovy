c514a67e16c3505352757c5c7019abfda628aab1
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-08 14:59:17 +0000

    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@14 c3b44e55-a100-42f6-9c86-32dbf579054b

---
+++ grails-app/services/bartsy/ApplePNService.groovy
@@ -0,0 +1,26 @@
+package bartsy
+
+import com.notnoop.apns.ApnsService
+import com.notnoop.apns.APNS
+import grails.converters.JSON
+
+class ApplePNService {
+
+    def sendPN(String orderStatus,String orderId,String token,String bad,String body) {
+		println "in Apple PN"
+		ApnsService service = APNS.newService()
+		//.withCert("/home/swethab/swetha/Bartsy/Certificates.p12", "123456")
+		.withCert("/usr/local/Bartsy/Certificates.p12", "123456")
+		.withSandboxDestination()
+		.build();
+		
+		
+		
+		String payload = APNS.newPayload().alertBody(body)
+				.badge(Integer.parseInt(bad))
+				.customField("orderStatus", orderStatus)
+				.customField("orderId", orderId)
+				.sound("default").build();
+		service.push(token, payload);
+	}
+}
