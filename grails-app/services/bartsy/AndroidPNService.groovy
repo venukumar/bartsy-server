bc6ae134780ba7fd5323ae31c6b098681bc7bddf
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-09 14:58:22 +0000

    New method to send User profiles. Added message type paramter to all methods.
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@24 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/services/bartsy/AndroidPNService.groovy
+++ grails-app/services/bartsy/AndroidPNService.groovy
@@ -6,7 +6,7 @@ import com.google.android.gcm.server.Sender
 
 class AndroidPNService {
 
-    public void sendPN(String orderStatus,String orderId,String registrationId){
+    public void sendPN(String orderStatus,String orderId,String registrationId,String messageType){
         try {
             log.info("Came into the android push")
 			println "Came into the android push"
@@ -20,6 +20,7 @@ class AndroidPNService {
 //            .delayWhileIdle(true)
             .addData("orderStatus",orderStatus)
             .addData("orderId",orderId)
+			.addData("messageType",messageType)
             .addData("badgeCount",String.valueOf(1))
             .build();
             Result result = sender.send(message,registrationId,1);
@@ -29,7 +30,7 @@ class AndroidPNService {
         }
     }
 	
-	public void sendPlaceOrderPN(String orderStatus,String orderId,String itemName,String orderTime,String basePrice,String tipPercentage,String totalPrice,String registrationId){
+	public void sendPlaceOrderPN(String orderStatus,String orderId,String itemName,String orderTime,String basePrice,String tipPercentage,String totalPrice,String registrationId,String messageType){
 		try {
 			log.info("Came into the android push")
 			//println "Came into the android push"
@@ -49,6 +50,7 @@ class AndroidPNService {
 			.addData("tipPercentage",tipPercentage)
 			.addData("totalPrice",totalPrice)
 			.addData("badgeCount",String.valueOf(1))
+			.addData("messageType",messageType)
 			.build();
 			Result result = sender.send(message,registrationId,1);
 			log.info("Result---------->>>"+result.toString())
@@ -56,5 +58,31 @@ class AndroidPNService {
 			log.info("Android PN Exception"+e.getMessage())
 		}
 	}
+	
+	public void sendUserProfilePN(Map userProfileMap,String registrationId,String messageType){
+		try {
+			log.info("Came into the android push")
+			//println "Came into the android push"
+			//String apiKey = "AIzaSyD8HRgmmsbrAqBOFQGngf5MkHYG1qacj9Q" // development
+			String apiKey = "AIzaSyBXDQMxqmGHJ7fOQeysFDFmrZRpuI1vp40" // production
+			Sender sender = new Sender(apiKey)
+			Message message
+			message = new Message.Builder()
+//            .collapseKey("1")
+//            .timeToLive(3)
+//            .delayWhileIdle(true)
+			.addData("bartsyId",userProfileMap.get(bartsyId))
+			.addData("gender",userProfileMap.get(gender))
+			.addData("name",userProfileMap.get(name))
+			.addData("badgeCount",String.valueOf(1))
+			.addData("messageType",messageType)
+			.build();
+			Result result = sender.send(message,registrationId,1);
+			log.info("Result---------->>>"+result.toString())
+		} catch (Exception e) {
+			log.info("Android PN Exception"+e.getMessage())
+		}
+	}
+
 
 }
