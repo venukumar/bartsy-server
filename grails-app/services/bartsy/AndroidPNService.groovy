7a5d0900118f951932add65bb20dfe0cb2369126
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-20 06:03:41 +0000

    Added comments to each webservice and cleaned up the code
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@46 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/services/bartsy/AndroidPNService.groovy
+++ grails-app/services/bartsy/AndroidPNService.groovy
@@ -10,18 +10,11 @@ class AndroidPNService {
 	public void sendPN(Map pnMessage,String registrationId){
 		try {
 			log.info("Came into the android push")
-			println "Came into the android push"
-			println "message:"+pnMessage.toString()
-			println "message as JSON: "+ (pnMessage as JSON).toString()
 			//String apiKey = "AIzaSyD8HRgmmsbrAqBOFQGngf5MkHYG1qacj9Q" // development
 			String apiKey = "AIzaSyCg5JuXmUMUrdUjNXZFNOncozO3vuUaeak" // production
 			Sender sender = new Sender(apiKey)
 			Message message
 			message = new Message.Builder()
-					//            .collapseKey("1")
-					//            .timeToLive(3)
-					//            .delayWhileIdle(true)
-
 					.addData("badgeCount",String.valueOf(1))
 					.addData("message",(pnMessage as JSON).toString())
 					.build();
@@ -31,56 +24,4 @@ class AndroidPNService {
 			log.info("Android PN Exception"+e.getMessage())
 		}
 	}
-
-//	public void sendPlaceOrderPN(String orderStatus,String orderId,String itemName,String orderTime,String basePrice,String tipPercentage,String totalPrice,String registrationId,String messageType){
-//		try {
-//			log.info("Came into the android push")
-//			//println "Came into the android push"
-//			//String apiKey = "AIzaSyD8HRgmmsbrAqBOFQGngf5MkHYG1qacj9Q" // development
-//			String apiKey = "AIzaSyDC5CeQRHl3mE7pE9BLemmn1oWhhFxwvVk" // production
-//			Sender sender = new Sender(apiKey)
-//			
-//			Message message
-//			message = new Message.Builder()
-//					//            .collapseKey("1")
-//					//            .timeToLive(3)
-//					//            .delayWhileIdle(true)
-//					.addData("badgeCount",String.valueOf(1))
-//					.addData("message",json.toString())
-//					.build();
-//
-//
-//			System.out.println("message "+message);
-//			Result result = sender.send(message,registrationId,1);
-//			println("result.toString( "+result.toString());
-//			log.info("Result---------->>>"+result.toString())
-//		} catch (Exception e) {
-//			log.info("Android PN Exception"+e.getMessage())
-//		}
-//	}
-//
-//	public void sendUserProfilePN(Map userProfileMap,String registrationId,String messageType){
-//		try {
-//			log.info("Came into the android push")
-//			//println "Came into the android push"
-//			//String apiKey = "AIzaSyD8HRgmmsbrAqBOFQGngf5MkHYG1qacj9Q" // development
-//			String apiKey = "AIzaSyDC5CeQRHl3mE7pE9BLemmn1oWhhFxwvVk" // production
-//			Sender sender = new Sender(apiKey)
-//			Message message
-//			message = new Message.Builder()
-//					//            .collapseKey("1")
-//					//            .timeToLive(3)
-//					//            .delayWhileIdle(true)
-//					.addData("bartsyId",userProfileMap.get(bartsyId))
-//					.addData("gender",userProfileMap.get(gender))
-//					.addData("name",userProfileMap.get(name))
-//					.addData("badgeCount",String.valueOf(1))
-//					.addData("messageType",messageType)
-//					.build();
-//			Result result = sender.send(message,registrationId,1);
-//			log.info("Result---------->>>"+result.toString())
-//		} catch (Exception e) {
-//			log.info("Android PN Exception"+e.getMessage())
-//		}
-//	}
 }
