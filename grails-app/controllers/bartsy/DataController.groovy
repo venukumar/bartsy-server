9d2c47af09781726fc7cb3e4ba86b5558e491327
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-24 13:02:39 +0000

    Changed syncCustomerApp webservice to getUserOrders which will now return the list of any open orders for that particular user. Added userImagePath in the response of checkedInUsersList webservice. Added syncBartenderApp webservice which will return list of checkedInUsers in that venue and open orders for that venue.
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@53 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/controllers/bartsy/DataController.groovy
+++ grails-app/controllers/bartsy/DataController.groovy
@@ -23,26 +23,27 @@ class DataController {
 	 * @return  }
 	 *
 	 **/
-	def syncCustomerApp = {
+	def getUserOrders = {
 		def json = JSON.parse(request)
 		def bartsyId = json.bartsyId
 		def response = [:]
 		def totalOrders = []
 		def userProfile = UserProfile.findByBartsyId(bartsyId)
 		if(userProfile){
-			def checkedInto = CheckedInUsers.findByUserProfileAndStatus(userProfile,1)
-			if(checkedInto){
-				response.put("errorCode","0")
-				response.put("venueId",checkedInto.venue.venueId)
-				response.put("venueName",checkedInto.venue.venueName)
-				def openOrders = Orders.findAllByUserAndOrderStatusNotEqual(userProfile,"0")
+				//def openOrders = Orders.findAllByUserAndOrderStatusNotEqual(userProfile,"0")
+				def openOrdersCriteria = Orders.createCriteria()
+				def openOrders = openOrdersCriteria.list {
+					eq("user",userProfile)
+					and{
+						'in'("orderStatus",["0","2","3"])
+					}
+				}
 				if(openOrders){
-					println openOrders.size()
 					openOrders.each{
 						def order = it
 						def orderMap = [:]
 						orderMap.put("orderId",order.orderId)
-						orderMap.put("orderTime",order.orderTime)
+						orderMap.put("orderTime",order.dateCreated.toGMTString())
 						orderMap.put("orderStatus",order.orderStatus)
 						orderMap.put("basePrice",order.basePrice)
 						orderMap.put("totalPrice",order.totalPrice)
@@ -50,16 +51,12 @@ class DataController {
 						orderMap.put("itemName",order.itemName)
 						orderMap.put("itemId",order.itemId)
 						orderMap.put("description",order.description)
+						orderMap.put("updateTime",order.lastUpdated.toGMTString())
 						totalOrders.add(orderMap)
 					}
 					response.put("orders",totalOrders)
 				}
 			}
-			else{
-				response.put("errorCode","1")
-				response.put("errorMessage","User not checked into any venue")
-			}
-		}
 		else{
 			response.put("errorCode","1")
 			response.put("errorMessage","User Does not exist")
@@ -86,19 +83,18 @@ class DataController {
 	def checkedInUsersList = {
 		def json = JSON.parse(request)
 		def venueId = json.venueId
-		println venueId
 		def response = [:]
 		def checkedInUsersList = []
 		def venue = Venue.findByVenueId(venueId.toString())
 		if(venue){
 			def checkedInUsers = CheckedInUsers.findAllByVenue(venue)
 			if(checkedInUsers){
-				println checkedInUsers.size()
 				checkedInUsers.each{
 					def checkedInUser = it
 					def checkedInUsersMap = [:]
+					checkedInUsersMap.put("bartsyId",checkedInUser.userProfile.bartsyId.toString())
 					checkedInUsersMap.put("name",checkedInUser.userProfile.name)
-					checkedInUsersMap.put("userImage",checkedInUser.userProfile.userImage)
+					checkedInUsersMap.put("userImagePath",checkedInUser.userProfile.userImage)
 					checkedInUsersMap.put("gender",checkedInUser.userProfile.gender)
 					checkedInUsersList.add(checkedInUsersMap)
 				}
@@ -107,6 +103,78 @@ class DataController {
 			else{
 				response.put("errorCode","1")
 				response.put("errorMessage","No Checked in Users")
+			}
+		}
+		else{
+			response.put("errorCode","1")
+			response.put("errorMessage","Venue Does not exist")
+		}
+		render(text:response as JSON ,  contentType:"application/json")
+	}
+	
+	/**
+	 * This is the webservice to sync the data upon bartender app start up
+	 *
+	 * @author Swetha Bhatnagar
+	 *
+	 * @errorCodes 1 : failure, 0 : success
+	 *
+	 * @param venueId				server generated id of the venue
+	 *
+	 * @return  {
+	 * @return      errorCode 		: success/failure code
+	 * @return      errorMessage 	: success/failure message
+	 * @return      venueId 		: venue id where user was checked into
+	 * @return      venueName 		: name of the venue where the user was checked into
+	 * @return      orders			: orders list placed by the user
+	 * @return  }
+	 *
+	 **/
+	def syncBartenderApp = {
+		def json = JSON.parse(request)
+		def venue = Venue.findByVenueId(json.venueId)
+		def checkedInUsersList = []
+		def ordersList = []
+		def response = [:]
+		if(venue){
+			def checkedInUsers = CheckedInUsers.findAllByVenue(venue)
+			if(checkedInUsers){
+				checkedInUsers.each{
+					def checkedInUser = it
+					def checkedInUsersMap = [:]
+					checkedInUsersMap.put("bartsyId",checkedInUser.userProfile.bartsyId)
+					checkedInUsersMap.put("name",checkedInUser.userProfile.name)
+					checkedInUsersMap.put("userImagePath",checkedInUser.userProfile.userImage)
+					checkedInUsersMap.put("gender",checkedInUser.userProfile.gender)
+					checkedInUsersList.add(checkedInUsersMap)
+				}
+				response.put("checkedInUsers",checkedInUsersList)
+			}
+			//def orders = Orders.findAllByVenue(venue)
+			def ordersCriteria = Orders.createCriteria()
+			def orders = ordersCriteria.list{
+				eq("venue",venue)
+				and{
+						'in'("orderStatus",["0","2","3"])
+					}
+			}	
+			if(orders){
+				orders.each{
+					def order = it
+					def ordersMap = [:]
+					ordersMap.put("bartsyId",order.user.bartsyId)
+					ordersMap.put("orderStatus",order.orderStatus)
+					ordersMap.put("orderId",order.orderId)
+					ordersMap.put("itemName",order.itemName)
+					ordersMap.put("orderTime",order.dateCreated.toGMTString())
+					ordersMap.put("basePrice",order.basePrice)
+					ordersMap.put("tipPercentage",order.tipPercentage)
+					ordersMap.put("totalPrice", order.totalPrice)
+					ordersMap.put("description",order.description)
+					ordersMap.put("updateTime",order.lastUpdated.toGMTString())
+					ordersList.add(ordersMap)
+				}
+				response.put("orders",ordersList)
 			}
 		}
 		else{
