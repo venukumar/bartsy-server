1944cc6de2dda56abb30d5e3b285df1e13df96dc
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-29 13:09:35 +0000

    Modified table to store more profile data like orientation etc.
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@59 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/domain/bartsy/UserProfile.groovy
+++ grails-app/domain/bartsy/UserProfile.groovy
@@ -2,23 +2,42 @@ package bartsy
 
 class UserProfile {
 
-	static constraints = {
+     static constraints = {
+         
+    }
+	
+	static mapping = {
+		// userImage(sqlType:'blob')
 	}
-
-	String name
-	//String firstName
-	//String lastName
-	//String relationshipStatus
-	//String orientation
-	//Date DOB
-	String userImage
-	String loginId
-	long bartsyId
-	String loginType
-	String userName
-	String gender
-	int deviceType
-	String deviceToken
-	Date dateCreated
-	Date lastUpdated
-}
+        
+   // static belongsTo = [bartsyUserDetails:BartsyUserDetails]
+	
+        String name
+        String firstName
+		String lastName
+		String dateOfBirth
+		String description
+		String orientation
+		String status
+		String nickName
+	//String location
+	//String emailAddress
+	//String mobileNumber
+	//String preference
+        //String gender
+        //String relationshipStatus
+        //String userStatus
+       // String language1
+        //String language2
+        String userImage
+		String loginId
+		long bartsyId
+		String loginType
+		String userName
+		String gender
+		int deviceType
+		String deviceToken
+		Date dateCreated
+		Date lastUpdated
+		String showProfile		
+      }
