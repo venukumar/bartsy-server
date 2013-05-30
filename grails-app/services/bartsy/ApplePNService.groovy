427ce6afec3e8533c3ebb860d668755d18f0acc5
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-30 08:49:53 +0000

    Added different heartBeat methods for userTimeout, heartBeat and orderTimeout PN.
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@67 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/services/bartsy/ApplePNService.groovy
+++ grails-app/services/bartsy/ApplePNService.groovy
@@ -8,8 +8,8 @@ class ApplePNService {
 
 	def sendPN(Map pnMessage,String token,String bad,String body) {
 		ApnsService service = APNS.newService()
-				//.withCert("/home/swethab/swetha/Bartsy/Certificates.p12", "123456")
-				.withCert("/usr/local/Bartsy/Certificates.p12", "123456")
+				.withCert("/home/swethab/swetha/Bartsy/Certificates.p12", "123456")
+				//.withCert("/usr/local/Bartsy/Certificates.p12", "123456")
 				.withSandboxDestination()
 				.build();
 
@@ -18,7 +18,57 @@ class ApplePNService {
 				.customField("orderStatus", pnMessage.get("orderStatus"))
 				.customField("orderId", pnMessage.get("orderId"))
 				.customField("messageType", pnMessage.get("messageType"))
-				.sound("default").build();
+				.sound("default")
+				.build();
+		service.push(token, payload);
+	}
+	
+	def sendPNOrderTimeout(Map pnMessage,String token,String bad,String body) {
+		ApnsService service = APNS.newService()
+				.withCert("/home/swethab/swetha/Bartsy/Certificates.p12", "123456")
+				//.withCert("/usr/local/Bartsy/Certificates.p12", "123456")
+				.withSandboxDestination()
+				.build();
+
+		String payload = APNS.newPayload().alertBody(body)
+				.badge(Integer.parseInt(bad))
+				.customField("cancelledOrder", pnMessage.get("cancelledOrder"))
+				.customField("messageType", pnMessage.get("messageType"))
+				.sound("default")
+				.build();
+		service.push(token, payload);
+	}
+	
+	def sendPNHeartBeat(Map pnMessage,String token,String bad,String body) {
+		ApnsService service = APNS.newService()
+				.withCert("/home/swethab/swetha/Bartsy/Certificates.p12", "123456")
+				//.withCert("/usr/local/Bartsy/Certificates.p12", "123456")
+				.withSandboxDestination()
+				.build();
+
+		String payload = APNS.newPayload()//.alertBody(body)
+				.badge(Integer.parseInt(bad))
+				.customField("checkedInUsersList", pnMessage.get("checkedInUsersList"))
+				.customField("ordersList", pnMessage.get("ordersList"))
+				.customField("messageType", pnMessage.get("messageType"))
+				//.sound("default")
+				.build();
+		service.push(token, payload);
+	}	
+	
+	def sendPNUserTimeout(Map pnMessage,String token,String bad,String body) {
+		ApnsService service = APNS.newService()
+				.withCert("/home/swethab/swetha/Bartsy/Certificates.p12", "123456")
+				//.withCert("/usr/local/Bartsy/Certificates.p12", "123456")
+				.withSandboxDestination()
+				.build();
+
+		String payload = APNS.newPayload()//.alertBody(body)
+				.badge(Integer.parseInt(bad))
+				.customField("bartsyId", pnMessage.get("bartsyId"))
+				.customField("messageType", pnMessage.get("messageType"))
+				//.sound("default")
+				.build();
 		service.push(token, payload);
 	}
 }
