74170039d342d0e5496f8635e1fcef6d071d0b9c
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-06-05 05:48:16 +0000

    Added columns authTransactionId and payTransactionId
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@90 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/domain/bartsy/Orders.groovy
+++ grails-app/domain/bartsy/Orders.groovy
@@ -3,6 +3,8 @@ package bartsy
 class Orders {
 
     static constraints = {
+		authTransactionId(nullable:true)
+		payTransactionId(nullable:true)
     }
     
     static belongsTo = [user:UserProfile,venue:Venue]
@@ -16,5 +18,7 @@ class Orders {
 	String itemId
 	long orderId
 	String description
+	long authTransactionId
+	long payTransactionId
 	Date lastUpdated
 }
