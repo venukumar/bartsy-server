cf0a4f482504715503607117bdb8c076a21b95de
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-06-11 06:44:56 +0000

    Added payment related methods
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@112 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/services/bartsy/PaymentService.groovy
+++ grails-app/services/bartsy/PaymentService.groovy
@@ -1,32 +1,86 @@
 package bartsy
 
-import com.vinomis.authnet.AuthorizeNet
+
 import grails.converters.JSON
+import net.authorize.Merchant
+import net.authorize.data.creditcard.CreditCard
+import net.authorize.Environment
+import net.authorize.aim.Result
+import net.authorize.aim.Transaction
+import net.authorize.TransactionType
+import net.authorize.data.creditcard.CardType
 
 class PaymentService {
 
-	def authorizePayment() {
-		def s = new AuthorizeNet()
-		s.authorizeOnly {
-			amount '1.00'
-			ccNumber '370000000000002'
-			cvv '122'
-			ccExpDate '012014'
-			email 'bhatnagar.swetha@gmail.com'
-			invoiceId '123'
-		}
-		def anr = s.submit()
-		return anr
-	}
 
-	def capturePayment(){
-		def s = new AuthorizeNet()
-		s.capturePriorAuthorization {
-			amount '1.00'  // Only needed if this amount is less than the original authorization. It cannot be more.
-			txid '2194065140'  //TODO: Replace this txId
+	def authorizePayment(){
+		def response = [:]
+		Merchant merchant = Merchant.createMerchant(Environment.SANDBOX,
+				"75x2yLLj", "5Lq4dG24m63qncQ4");
+		// create credit card
+		CreditCard creditCard = CreditCard.createCreditCard();
+		creditCard.setCreditCardNumber("4007000000027");
+		creditCard.setExpirationMonth("12");
+		creditCard.setExpirationYear("2015");
+		// create transaction
+		Transaction authCaptureTransaction = merchant.createAIMTransaction(
+				TransactionType.AUTH_ONLY, new BigDecimal(1.99));
+		authCaptureTransaction.setCreditCard(creditCard);
+		Result<Transaction> result = (Result<Transaction>) merchant.postTransaction(authCaptureTransaction);
+		if(result.isApproved()) {
+			response.put("authApproved","true")
+			response.put("authCode",result.getTarget().getAuthorizationCode())	
+			response.put("authTransactionNumber",result.getTarget().getTransactionId())			
 		}
-		def anr = s.submit()
-		println anr
+		else{
+			response.put("authApproved","false")
+			response.put("authErrorMessage",result.getResponseText())
+		}		
+		println response
+		return response
+	}
+	def capturePayment(def authCode){
+		def response = [:]
+		Merchant merchant = Merchant.createMerchant(Environment.SANDBOX,
+				"75x2yLLj", "5Lq4dG24m63qncQ4");
+		// create credit card
+		CreditCard creditCard = CreditCard.createCreditCard();
+		creditCard.setCreditCardNumber("4007000000027");
+		creditCard.setExpirationMonth("12");
+		creditCard.setExpirationYear("2015");
+		// create transaction
+		Transaction authCaptureTransaction = merchant.createAIMTransaction(
+				TransactionType.CAPTURE_ONLY, new BigDecimal(1.99));
+		authCaptureTransaction.setAuthorizationCode(authCode)
+		authCaptureTransaction.setCreditCard(creditCard);
+		Result<Transaction> result = (Result<Transaction>) merchant.postTransaction(authCaptureTransaction);
+		if(result.isApproved()) {
+			response.put("captureApproved","true")
+			response.put("captureTransactionNumber",result.getTarget().getTransactionId())
+		}
+		else{
+			response.put("captureApproved","false")
+			response.put("captureErrorMessage",result.getResponseText())
+		}
+		println response
+		return response
 	}
 	
+	def makePayment(order,status){
+		def captureResponse = capturePayment(order.getAuthCode())
+		if(captureResponse.get("captureApproved").toBoolean()){
+			order.setCaptureApproved("true")
+			order.setOrderStatus(status)
+			order.setCaptureTransactionNumber(captureResponse.get("captureTransactionNumber"))
+		}
+		else{
+			order.setOrderStatus("7")
+			order.setCaptureApproved("false")
+			order.setCaptureErrorMessage(captureResponse.get("captureErrorMessage"))
+		}
+		order.save()
+		return response
+	}
+
 }
+
