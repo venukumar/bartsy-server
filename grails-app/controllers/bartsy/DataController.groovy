8c154bf207237f95d0d08334c4d7eb3f46877965
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-06-14 13:46:26 +0000

    Made changes to check for apiVersion and changed bartsyId from long to String. Also changed receiverbartsyId from long to String
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@129 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/controllers/bartsy/DataController.groovy
+++ grails-app/controllers/bartsy/DataController.groovy
@@ -24,42 +24,61 @@ class DataController {
 	 *
 	 **/
 	def getUserOrders = {
-		def json = JSON.parse(request)
-		def bartsyId = json.bartsyId
 		def response = [:]
-		def totalOrders = []
-		def userProfile = UserProfile.findByBartsyId(bartsyId)
-		if(userProfile){
-				//def openOrders = Orders.findAllByUserAndOrderStatusNotEqual(userProfile,"0")
-				def openOrdersCriteria = Orders.createCriteria()
-				def openOrders = openOrdersCriteria.list {
-					eq("user",userProfile)
-					and{
-						'in'("orderStatus",["0","2","3"])
+		try{
+			def json = JSON.parse(request)
+			def apiVersion = BartsyConfiguration.findByConfigName("apiVersion")
+			if(apiVersion.value.toInteger() == json.apiVersion.toInteger()){
+				def bartsyId = json.bartsyId
+				def totalOrders = []
+				def userProfile = UserProfile.findByBartsyId(bartsyId.toString())
+				if(userProfile){
+					//def openOrders = Orders.findAllByUserAndOrderStatusNotEqual(userProfile,"0")
+					def openOrdersCriteria = Orders.createCriteria()
+					def openOrders = openOrdersCriteria.list {
+						'in'("orderStatus",["0", "2", "3"])
+						or{
+							eq("user",userProfile)
+							eq("recieverBartsyId",userProfile.bartsyId)
+						}
+					}
+					if(openOrders){
+						openOrders.each{
+							def order = it
+							def orderMap = [:]
+							orderMap.put("orderId",order.orderId)
+							orderMap.put("senderBartsyId",order.user.bartsyId)
+							orderMap.put("recieverBartsyId",order.recieverBartsyId)
+							orderMap.put("drinkOffered",order.drinkOffered)
+							orderMap.put("orderTime",order.dateCreated.toGMTString())
+							orderMap.put("orderStatus",order.orderStatus)
+							orderMap.put("basePrice",order.basePrice)
+							orderMap.put("totalPrice",order.totalPrice)
+							orderMap.put("tipPercentage",order.tipPercentage)
+							orderMap.put("itemName",order.itemName)
+							orderMap.put("itemId",order.itemId)
+							orderMap.put("description",order.description)
+							orderMap.put("updateTime",order.lastUpdated.toGMTString())
+							orderMap.put("specialInstructions",order.specialInstructions)
+							totalOrders.add(orderMap)
+						}
+						response.put("orders",totalOrders)
 					}
 				}
-				if(openOrders){
-					openOrders.each{
-						def order = it
-						def orderMap = [:]
-						orderMap.put("orderId",order.orderId)
-						orderMap.put("orderTime",order.dateCreated.toGMTString())
-						orderMap.put("orderStatus",order.orderStatus)
-						orderMap.put("basePrice",order.basePrice)
-						orderMap.put("totalPrice",order.totalPrice)
-						orderMap.put("tipPercentage",order.tipPercentage)
-						orderMap.put("itemName",order.itemName)
-						orderMap.put("itemId",order.itemId)
-						orderMap.put("description",order.description)
-						orderMap.put("updateTime",order.lastUpdated.toGMTString())
-						totalOrders.add(orderMap)
-					}
-					response.put("orders",totalOrders)
+				else{
+					response.put("errorCode","1")
+					response.put("errorMessage","User Does not exist")
 				}
 			}
-		else{
-			response.put("errorCode","1")
-			response.put("errorMessage","User Does not exist")
+			else{
+				response.put("errorCode","100")
+				response.put("errorMessage","API version do not match")
+			}
+		}
+		catch(Exception e){
+			log.info("Exception is ===> "+e.getMessage())
+			response.put("errorCode",200)
+			response.put("errorMessage",e.getMessage())
 		}
 		render(text:response as JSON ,  contentType:"application/json")
 	}
@@ -81,43 +100,56 @@ class DataController {
 	 *
 	 **/
 	def checkedInUsersList = {
-		def json = JSON.parse(request)
-		def venueId = json.venueId
 		def response = [:]
-		def checkedInUsersList = []
-		def venue = Venue.findByVenueId(venueId.toString())
-		if(venue){
-			def checkedInUsers = CheckedInUsers.findAllByVenueAndStatus(venue,1)
-			if(checkedInUsers){
-				checkedInUsers.each{
-					def checkedInUser = it
-					if(checkedInUser.userProfile.showProfile.equals("ON")){
-					def checkedInUsersMap = [:]
-					checkedInUsersMap.put("bartsyId",checkedInUser.userProfile.bartsyId.toString())
-					checkedInUsersMap.put("nickName",checkedInUser.userProfile.nickName)
-					checkedInUsersMap.put("userImagePath",checkedInUser.userProfile.userImage)
-					checkedInUsersMap.put("gender",checkedInUser.userProfile.gender)
-					checkedInUsersMap.put("orientation",checkedInUser.userProfile.orientation)
-					checkedInUsersMap.put("status",checkedInUser.userProfile.status)
-					checkedInUsersMap.put("description",checkedInUser.userProfile.description)
-					checkedInUsersMap.put("dateOfBirth",checkedInUser.userProfile.dateOfBirth)
-					checkedInUsersList.add(checkedInUsersMap)
+		try{
+			def json = JSON.parse(request)
+			def apiVersion = BartsyConfiguration.findByConfigName("apiVersion")
+			if(apiVersion.value.toInteger() == json.apiVersion.toInteger()){
+				def venueId = json.venueId
+				def checkedInUsersList = []
+				def venue = Venue.findByVenueId(venueId.toString())
+				if(venue){
+					def checkedInUsers = CheckedInUsers.findAllByVenueAndStatus(venue,1)
+					if(checkedInUsers){
+						checkedInUsers.each{
+							def checkedInUser = it							
+								def checkedInUsersMap = [:]
+								checkedInUsersMap.put("bartsyId",checkedInUser.userProfile.bartsyId.toString())
+								checkedInUsersMap.put("nickName",checkedInUser.userProfile.nickName)
+								checkedInUsersMap.put("userImagePath",checkedInUser.userProfile.userImage)
+								checkedInUsersMap.put("gender",checkedInUser.userProfile.gender)
+								checkedInUsersMap.put("orientation",checkedInUser.userProfile.orientation)
+								checkedInUsersMap.put("status",checkedInUser.userProfile.status)
+								checkedInUsersMap.put("description",checkedInUser.userProfile.description)
+								checkedInUsersMap.put("dateOfBirth",checkedInUser.userProfile.dateOfBirth)
+								checkedInUsersMap.put("showProfile",checkedInUser.userProfile.showProfile)
+								checkedInUsersList.add(checkedInUsersMap)						
+						}
+						response.put("checkedInUsers",checkedInUsersList)
+					}
+					else{
+						response.put("errorCode","1")
+						response.put("errorMessage","No Checked in Users")
 					}
 				}
-				response.put("checkedInUsers",checkedInUsersList)
+				else{
+					response.put("errorCode","1")
+					response.put("errorMessage","Venue Does not exist")
+				}
 			}
 			else{
-				response.put("errorCode","1")
-				response.put("errorMessage","No Checked in Users")
+				response.put("errorCode","100")
+				response.put("errorMessage","API version do not match")
 			}
 		}
-		else{
-			response.put("errorCode","1")
-			response.put("errorMessage","Venue Does not exist")
+		catch(Exception e){
+			log.info("Exception is ===> "+e.getMessage())
+			response.put("errorCode",200)
+			response.put("errorMessage",e.getMessage())
 		}
 		render(text:response as JSON ,  contentType:"application/json")
 	}
-	
+
 	/**
 	 * This is the webservice to sync the data upon bartender app start up
 	 *
@@ -137,70 +169,116 @@ class DataController {
 	 *
 	 **/
 	def syncBartenderApp = {
-		def json = JSON.parse(request)
-		def venue = Venue.findByVenueId(json.venueId.toString())
-		def checkedInUsersList = []
-		def ordersList = []
 		def response = [:]
-		if(venue){
-			response.put("venueStatus",venue.status)
-			def checkedInUsers = CheckedInUsers.findAllByVenueAndStatus(venue,1)
-			if(checkedInUsers){
-				checkedInUsers.each{
-					def checkedInUser = it
-					def checkedInUsersMap = [:]
-					checkedInUsersMap.put("bartsyId",checkedInUser.userProfile.bartsyId)
-					checkedInUsersMap.put("name",checkedInUser.userProfile.name)
-					checkedInUsersMap.put("userImagePath",checkedInUser.userProfile.userImage)
-					checkedInUsersMap.put("gender",checkedInUser.userProfile.gender)
-					checkedInUsersList.add(checkedInUsersMap)
-				}
-				response.put("checkedInUsers",checkedInUsersList)
-			}
-			//def orders = Orders.findAllByVenue(venue)
-			def ordersCriteria = Orders.createCriteria()
-			def orders = ordersCriteria.list{
-				eq("venue",venue)
-				and{
-						'in'("orderStatus",["0","2","3"])
+		try{
+			def json = JSON.parse(request)
+			def apiVersion = BartsyConfiguration.findByConfigName("apiVersion")
+			if(apiVersion.value.toInteger() == json.apiVersion.toInteger()){
+				def venue = Venue.findByVenueId(json.venueId.toString())
+				def checkedInUsersList = []
+				def ordersList = []
+				if(venue){
+					response.put("venueStatus",venue.status)
+					def checkedInUsers = CheckedInUsers.findAllByVenueAndStatus(venue,1)
+					if(checkedInUsers){
+						checkedInUsers.each{
+							def checkedInUser = it
+							def checkedInUsersMap = [:]
+							checkedInUsersMap.put("bartsyId",checkedInUser.userProfile.bartsyId)
+							checkedInUsersMap.put("name",checkedInUser.userProfile.nickName)
+							checkedInUsersMap.put("userImagePath",checkedInUser.userProfile.userImage)
+							checkedInUsersMap.put("gender",checkedInUser.userProfile.gender)
+							checkedInUsersMap.put("showProfile",checkedInUser.userProfile.showProfile)
+							checkedInUsersList.add(checkedInUsersMap)
+						}
+						response.put("checkedInUsers",checkedInUsersList)
 					}
-			}	
-			if(orders){
-				orders.each{
-					def order = it
-					def ordersMap = [:]
-					ordersMap.put("bartsyId",order.user.bartsyId)
-					ordersMap.put("orderStatus",order.orderStatus)
-					ordersMap.put("orderId",order.orderId)
-					ordersMap.put("itemName",order.itemName)
-					ordersMap.put("orderTime",order.dateCreated.toGMTString())
-					ordersMap.put("basePrice",order.basePrice)
-					ordersMap.put("tipPercentage",order.tipPercentage)
-					ordersMap.put("totalPrice", order.totalPrice)
-					ordersMap.put("description",order.description)
-					ordersMap.put("updateTime",order.lastUpdated.toGMTString())
-					ordersList.add(ordersMap)
+					//def orders = Orders.findAllByVenue(venue)
+					def ordersCriteria = Orders.createCriteria()
+					def orders = ordersCriteria.list{
+						eq("venue",venue)
+						and{
+							'in'("orderStatus",["0", "2", "3"])
+						}
+					}
+					if(orders){
+						orders.each{
+							def order = it
+							def ordersMap = [:]
+							ordersMap.put("bartsyId",order.user.bartsyId)
+							ordersMap.put("orderStatus",order.orderStatus)
+							ordersMap.put("orderId",order.orderId)
+							ordersMap.put("itemName",order.itemName)
+							ordersMap.put("orderTime",order.dateCreated.toGMTString())
+							ordersMap.put("basePrice",order.basePrice)
+							ordersMap.put("tipPercentage",order.tipPercentage)
+							ordersMap.put("totalPrice", order.totalPrice)
+							ordersMap.put("description",order.description)
+							ordersMap.put("updateTime",order.lastUpdated.toGMTString())
+							ordersList.add(ordersMap)
+						}
+						response.put("orders",ordersList)
+					}
 				}
-				response.put("orders",ordersList)
+				else{
+					response.put("errorCode","1")
+					response.put("errorMessage","Venue Does not exist")
+				}
+			}
+			else{
+				response.put("errorCode","100")
+				response.put("errorMessage","API version do not match")
 			}
 		}
-		else{
-			response.put("errorCode","1")
-			response.put("errorMessage","Venue Does not exist")
+		catch(Exception e){
+			log.info("Exception is ===> "+e.getMessage())
+			response.put("errorCode",200)
+			response.put("errorMessage",e.getMessage())
+		}
+		render(text:response as JSON ,  contentType:"application/json")
+	}
+
+	def setConfigValues = {
+		def response = [:]
+		try{
+			def json = JSON.parse(request)
+			def apiVersion = BartsyConfiguration.findByConfigName("apiVersion")
+			if(apiVersion.value.toInteger() == json.apiVersion.toInteger()){
+				def userTimeout = json.userTimeout
+				def venueTimeout =  json.venueTimeout
+				def timer = BartsyConfiguration.findByConfigName("venueTimeout")
+				timer.setValue(venueTimeout)
+				timer.save()
+				timer=BartsyConfiguration.findByConfigName("userTimeout")
+				timer.setValue(userTimeout)
+				timer.save()
+				response.put("errorCode","0")
+				response.put("errorMessage","Success")
+			}
+			else{
+				response.put("errorCode","100")
+				response.put("errorMessage","API version do not match")
+			}
+		}
+		catch(Exception e){
+			log.info("Exception is ===> "+e.getMessage())
+			response.put("errorCode",200)
+			response.put("errorMessage",e.getMessage())
 		}
 		render(text:response as JSON ,  contentType:"application/json")
 	}
 	
-	def setConfigValues = {
+	def sendMail={
 		def json = JSON.parse(request)
-		def userTimeout = json.userTimeout
-		def venueTimeout =  json.venueTimeout
-		def timer = BartsyConfiguration.findByConfigName("venueTimeout")
-		timer.setValue(venueTimeout)
-		timer.save()
-		timer=BartsyConfiguration.findByConfigName("userTimeout")
-		timer.setValue(userTimeout)
-		timer.save()
-		render(text:'success',  contentType:"application/text")
+		def mailId=json.mailId
+		def message=json.message
+		println "mailID" +mailId
+		println "message"+message
+		println "forget password !!!!!!!!!!! "		
+		sendMail {
+			to mailId
+			subject "Hello Friend"
+			body message
+		 }
 	}
 }
