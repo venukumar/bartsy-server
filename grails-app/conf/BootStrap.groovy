11e522209fa4986c1822d1b019536ed8dd98ecbd
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-06-14 13:54:35 +0000

    added apiVersion entry while bootstrapping
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@139 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,11 +6,11 @@ class BootStrap {
 
     def init = { servletContext ->
 		if(!BartsyConfiguration.count()){
-			new BartsyConfiguration(configName:'timer',value:'false').save(flush:true)
+			new BartsyConfiguration(configName:'timer',value:'true').save(flush:true)
 			new BartsyConfiguration(configName:'heartbeat',value:'true').save(flush:true)
 			new BartsyConfiguration(configName:'userTimeout',value:'30').save(flush:true)
-			new BartsyConfiguration(configName:'venueTimeout',value:'15').save(flush:true)
-			
+			new BartsyConfiguration(configName:'venueTimeout',value:'30').save(flush:true)
+			new BartsyConfiguration(configName:'apiVersion',value:'1').save(flush:true)
 		}
 			  }
     def destroy = {
