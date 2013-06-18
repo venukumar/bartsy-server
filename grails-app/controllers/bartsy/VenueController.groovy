62a06eb2eb0c75af150f4ff4132314934863d1e3
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-06-18 08:22:30 +0000

    Commented the code properly
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@143 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/controllers/bartsy/VenueController.groovy
+++ grails-app/controllers/bartsy/VenueController.groovy
@@ -1,10 +1,17 @@
 package bartsy
 
-import java.nio.charset.MalformedInputException
 import grails.converters.JSON
 import org.json.simple.parser.JSONParser
 
+/**
+ * This is the controller which has all Venue related actions to be performed.
+ *
+ * @author Swetha Bhatnagar
+ *
+ **/
 class VenueController {
+	
+	def inventoryService
 
 	/**
 	 * This is a test webservice to save venue details from a hardcoded locu response. To be removed later. 
@@ -18,9 +25,10 @@ class VenueController {
 				def resources = grailsApplication.mainContext.getResource("response.txt").file
 				def fileContents = resources.text
 				def parsedData = JSON.parse(fileContents)
-				def hasBarSection
+				def hasBarSection = 0
 				def menu
-				Venue venue = Venue.findByLocuId(json.locuId)
+				Venue venue = Venue.getAll()
+				//Venue venue = Venue.findByLocuId(json.locuId)
 				if(venue){
 					venue.cancelOrderTime = json.cancelOrderTime as int
 					venue.deviceToken = json.deviceToken
@@ -29,7 +37,7 @@ class VenueController {
 					response.put("venueId",venue.getVenueId())
 					response.put("venueName",venue.getVenueName())
 					response.put("errorCode","1")
-					response.put("errorMessage","Venue already exists")
+					response.put("errorMessage","Venue Details Updated")
 				}
 				else{
 					parsedData.objects.menus.each {
@@ -76,7 +84,9 @@ class VenueController {
 						venue.wifiPassword = "XYZ"
 						venue.typeOfAuthentication = "XYZ"
 					}
+					if(hasBarSection){
 					venue.menu = menu
+					}
 					venue.deviceToken = json.deviceToken
 					venue.deviceType = json.deviceType
 					venue.paypalId = json.paypalId
@@ -97,7 +107,6 @@ class VenueController {
 						response.put("venueName",venue.getVenueName())
 						response.put("errorCode","0")
 						response.put("errorMessage","Save Successful")
-						startTimer()
 					}
 					else{
 						response.put("errorCode","1")
@@ -111,6 +120,7 @@ class VenueController {
 			}
 		}
 		catch(Exception e){
+			//if an exception occurs send errorCode 200 along with the exception message
 			log.info("Exception is ===> "+e.getMessage())
 			response.put("errorCode",200)
 			response.put("errorMessage",e.getMessage())
@@ -123,36 +133,35 @@ class VenueController {
 	 * 
 	 * @author Swetha Bhatnagar
 	 *
-	 * @errorCodes 1 : failure, 0 : success
-	 *
-	 * @param locuId         		locu.com registration id of the venue
-	 * @param deviceToken    		GCM registration id for Android and device id for IOS
-	 * @param wifiPresent    		flag to represent if wifi is present=1 at the venue
-	 * @param wifiName       		wifi name at the venue
-	 * @param wifiPassword   		wifi password at the venue
-	 * @param typeOfAuthentication  authentication tyoe for wifi is password or passphrase
-	 * @param deviceType		    deviceType is Android=0 or IOS=1
-	 * @param paypalId   			paypal id of the venue
-	 *
-	 * @return  {
-	 * @return      errorCode 		: success/failure code 
-	 * @return      errorMessage 	: success/failure message
-	 * @return      venueId 		: venue id generated for that venue
-	 * @return      venueName 		: name of the venue
-	 * @return  }
-	 *
 	 **/
 	def saveVenueDetails = {
+		//defining a map to return as a response for this syscall
 		def response = [:]
 		try{
+			//parse the request sent as input to the syscall
 			def json = JSON.parse(request)
+			//check to make sure the apiVersion sent in the request matches the correct apiVersion
 			def apiVersion = BartsyConfiguration.findByConfigName("apiVersion")
 			if(apiVersion.value.toInteger() == json.apiVersion.toInteger()){
-				def url = message(code:'app.locu.url')+json.locuId+'/?api_key='+message(code:'app.locu.apikey')
-				def parsedData = JSON.parse( new URL(url).text )
-				def hasBarSection
+				def parsedData
+				//check for the hardCoded locu Id for Finn McCool
+				if(json.locuId.equals("beec9320f3921035e4d7")){
+					//get the locu response into parsedData varibale for this venue from the harcoded locu response file
+					def resources = grailsApplication.mainContext.getResource("response.txt").file
+					def fileContents = resources.text
+					parsedData = JSON.parse(fileContents)
+				}
+				//if not Finn McCool go to else part
+				else{
+				//get the locu response for that locuID into the parsedData varibale
+					def url = message(code:'app.locu.url')+json.locuId+'/?api_key='+message(code:'app.locu.apikey')
+				parsedData = JSON.parse( new URL(url).text )
+				}
+				def hasBarSection = 0
 				def menu
+				//Get the venue based on the locu Id sent in the request
 				Venue venue = Venue.findByLocuId(json.locuId)
+				//if venue exists as of now updating the deviceToken and cancelOrderTime. To be changed later
 				if(venue){
 					venue.deviceToken = json.deviceToken
 					venue.cancelOrderTime = json.cancelOrderTime as int
@@ -160,22 +169,26 @@ class VenueController {
 					response.put("venueId",venue.getVenueId())
 					response.put("venueName",venue.getVenueName())
 					response.put("errorCode","1")
-					response.put("errorMessage",message(code:'venue.exists'))
+					response.put("errorMessage","Venue Details Updated")
 				}
+				//If venue does not exist go to else part
 				else{
+					//The following group of code is to parse the locu response and identify if it has a BAR section in its menu
 					parsedData.objects.menus.each {
 						def parsedData1 = it
 						parsedData1.each{
 							def parsedData2 = it
 							if(message(code:'bar').equals(parsedData2.menu_name.toString()))
 							{
+								//set this varibale to 1 if BAR section is there and encode the BAR menu JSON
 								hasBarSection = 1
 								menu = URLEncoder.encode(parsedData2.sections.toString(),"UTF-8")
 							}
 						}
 					}
-
+					//As venue does not exist create a new venue object
 					venue= new Venue()
+					//set the values to venue object from the locu response
 					venue.venueName = parsedData.objects[0].name
 					venue.lat = parsedData.objects[0].lat
 					venue.longtd = parsedData.objects[0].long
@@ -187,11 +200,16 @@ class VenueController {
 					venue.streetAddress = parsedData.objects[0].street_address
 					venue.websiteURL = parsedData.objects[0].website_url
 					venue.country = parsedData.objects[0].country
-					venue.hasLocuMenu = parsedData.objects[0].has_menu
-					venue.hasBarSection = hasBarSection
+					venue.hasLocuMenu = parsedData.objects[0].has_menu					
 					venue.twitterId = parsedData.objects[0].twitter_id
 					venue.facebookURL = parsedData.objects[0].facebook_url
 					venue.openHours = parsedData.objects[0].open_hours
+					//set the values of menu and hasBarSection based on the value set earlier
+					venue.hasBarSection = hasBarSection
+					if(hasBarSection){
+						venue.menu = menu
+						}
+					//set the values received in the request to this syscall
 					venue.cancelOrderTime = json.cancelOrderTime as int
 					venue.wifiPresent = json.wifiPresent
 					if(json.wifiPresent == 1){
@@ -203,14 +221,16 @@ class VenueController {
 						venue.wifiName = "XYZ"
 						venue.wifiPassword = "XYZ"
 						venue.typeOfAuthentication = "XYZ"
-					}
-					venue.menu = menu
+					}										
 					venue.deviceToken = json.deviceToken
 					venue.deviceType = json.deviceType
 					venue.paypalId = json.paypalId
+					//set the value of status as OPEN by default while registration
 					venue.status = "OPEN"
+					//inititally we set these values as current time...Later they get updated accordingly
 					venue.lastHBResponse = new Date()
 					venue.lastActivity =  new Date()
+					//get the latest venueId from DB and increase it by 1 and set it to the venue object
 					def maxId = Venue.createCriteria().get { projections { max "venueId" } } as Long
 					if(maxId){
 						maxId = maxId+1
@@ -219,52 +239,86 @@ class VenueController {
 						maxId = 100001
 					}
 					venue.venueId = maxId
-
+					//save the venue object to the DB
 					if(venue.save()){
+						//if save successful send the following details as response with errorCode 0
 						response.put("venueId",maxId)
 						response.put("venueName",venue.getVenueName())
 						response.put("errorCode","0")
 						response.put("errorMessage",message(code:'venue.save'))
-						startTimer()
 					}
 					else{
+						//if save not successful send the following details as response with errorCode 1
 						response.put("errorCode","1")
 						response.put("errorMessage",message(code:'venue.save.failed'))
 					}
 				}
 			}
 			else{
+				//if apiVersion do not match send errorCode 100
 				response.put("errorCode","100")
 				response.put("errorMessage","API version do not match")
 			}
 		}
 		catch(Exception e){
+			//if an exception occurs send errorCode 200 along with the exception message
 			log.info("Exception is ===> "+e.getMessage())
 			response.put("errorCode",200)
 			response.put("errorMessage",e.getMessage())
 		}
 		render(text:response as JSON ,  contentType:"application/json")
-
 	}
 
 
 	/**
-	 * This is the webservice to return the locu menu of a venue
-	 *
-	 * @author Swetha Bhatnagar
-	 *
-	 * @errorCodes 1 : failure, 0 : success
-	 *
-	 * @param venueId         		:server generated id for the venue
+	 * This is the webservice to return the menu received from locu for a venue.
 	 * 
-	 * @return  {
-	 * @return      errorCode 		: success/failure code
-	 * @return      errorMessage 	: success/failure message
-	 * @return      menu	 		: json format of the bar section of the locu menu of the venue
-	 * @return  }
+	 * @author Swetha Bhatnagar
 	 *
 	 **/
 	def getMenu = {
+		//defining a map to return as a response for this syscall
+		def response = [:]
+		try{
+			//parse the request sent as input to the syscall
+			def json = JSON.parse(request)
+			//check to make sure the apiVersion sent in the request matches the correct apiVersion
+			def apiVersion = BartsyConfiguration.findByConfigName("apiVersion")
+			if(apiVersion.value.toInteger() == json.apiVersion.toInteger()){
+				//retrieve the venueId from the parsed request and retrieve the venue based on venueId
+				def venueId = json.venueId
+				def venue = Venue.findByVenueId(venueId)
+				//check if venue exists with that venueId
+				if(venue){
+					//if venue exists retrieve the menu from that venue object and decode it to get the menu in JSON format
+					def menuJson = JSON.parse(URLDecoder.decode(venue.menu))
+					//send back the menu as response along with errorCode 0
+					response.put("menu",menuJson)
+					response.put("errorCode","0")
+					response.put("errorMessage",message(code:'venue.exists'))
+				}
+				else{
+					//if venue does not exist then send back errorCode 1
+					response.put("errorCode","1")
+					response.put("errorMessage",message(code:'venue.not.exists'))
+				}
+			}
+			else{
+				//if apiVersion do not match send errorCode 100
+				response.put("errorCode","100")
+				response.put("errorMessage","API version do not match")
+			}
+		}
+		catch(Exception e){
+			//if an exception occurs send errorCode 200 along with the exception message
+			log.info("Exception is ===> "+e.getMessage())
+			response.put("errorCode",200)
+			response.put("errorMessage",e.getMessage())
+		}
+		render(text:response as JSON,contentType:"application/json")
+	}
+	
+	def getMenuOld = {
 		def response = [:]
 		try{
 			def json = JSON.parse(request)
@@ -273,7 +327,40 @@ class VenueController {
 				def venueId = json.venueId
 				def venue = Venue.findByVenueId(venueId)
 				if(venue){
-					def menuJson = JSON.parse(URLDecoder.decode(venue.menu))
+					def menuJson = []
+					if(venue.hasBarSection){
+						menuJson = JSON.parse(URLDecoder.decode(venue.menu))
+					}
+					def cocktails = Cocktails.findAllByVenue(venue)
+					if(cocktails){
+						def sectionMap = [:]
+						sectionMap.put("section_name","Custom Cocktails")
+						def subSectionsList = []
+						def subSectionMap = [:]
+						def contentsList = []
+						def photos = []
+						def option_groups = []
+						subSectionMap.put("subsection_name","")
+						cocktails.each{
+							def cocktail = it
+							if(cocktail.getAvailable().toBoolean()){
+							def cocktailMap=[:]
+							// added all cocktail details cocktailsMap
+							cocktailMap.put("photos", photos)
+							cocktailMap.put("name", cocktail.name)
+							cocktailMap.put("description", "")
+							cocktailMap.put("option_groups", option_groups)
+							cocktailMap.put("price", cocktail.price)
+							cocktailMap.put("type", "ITEM")
+							// stored every cocktailsMap into cocktailsList
+							contentsList.add(cocktailMap)
+							}
+						}
+						subSectionMap.put("contents",contentsList)
+						subSectionsList.add(subSectionMap)
+						sectionMap.put("subsections",subSectionsList)
+						menuJson.add(sectionMap)
+					}
 					response.put("menu",menuJson)
 					response.put("errorCode","0")
 					response.put("errorMessage",message(code:'venue.exists'))
@@ -289,6 +376,7 @@ class VenueController {
 			}
 		}
 		catch(Exception e){
+			//if an exception occurs send errorCode 200 along with the exception message
 			log.info("Exception is ===> "+e.getMessage())
 			response.put("errorCode",200)
 			response.put("errorMessage",e.getMessage())
@@ -296,34 +384,43 @@ class VenueController {
 		render(text:response as JSON,contentType:"application/json")
 	}
 
+
 	/**
-	 * This is the webservice to return the list of venues registered.
-	 *
+	 * This is the webservice to return the list of registered venues.
+	 * 
 	 * @author Swetha Bhatnagar
 	 *
-	 * @errorCodes 1 : failure, 0 : success
-	 *
-	 * @return  Returns the venueName,venueId,latitude,longitude,address of all the venues registered     
 	 **/
 	def getVenueList = {
+		//defining a map to return as a response for this syscall
 		def response = [:]
 		try{
+			//parse the request sent as input to the syscall
 			def json = JSON.parse(request)
+			//check to make sure the apiVersion sent in the request matches the correct apiVersion
 			def apiVersion = BartsyConfiguration.findByConfigName("apiVersion")
 			if(apiVersion.value.toInteger() == json.apiVersion.toInteger()){
+				//get the list of venue Objects from DB
 				def venueList = Venue.getAll()
+				//define a list to add venue Objects
 				def totalVenueList = []
+				//check if atleast one venue exists
 				if(venueList){
+					//loop though the venue list
 					venueList.each{
 						def venue = it
+						//Find the list of checkedIn users for that venue
 						def checkedInUsers = CheckedInUsers.findAllByVenueAndStatus(venue,1)
+						//varibale to count number of private checked in users
 						def privateUsers = 0
+						//loop through the checkedin users list and check for private users
 						checkedInUsers.each{
 							def checkedInUser = it
 							if(checkedInUser.userProfile.getShowProfile().equals("OFF")){
 								privateUsers = privateUsers+1
 							}
 						}
+						//defining a map to store the venue object details
 						def venueMap = [:]
 						venueMap.put("checkedInUsers",checkedInUsers.size())
 						venueMap.put("privateUsers",privateUsers)
@@ -338,8 +435,10 @@ class VenueController {
 						venueMap.put("typeOfAuthentication",venue.getTypeOfAuthentication())
 						def address = venue.getStreetAddress()+","+venue.getLocality()+","+venue.getCountry()+","+venue.getPostalCode()
 						venueMap.put("address",address)
+						//add the venue object to the list defined earlier
 						totalVenueList.add(venueMap)
 					}
+					//send the list as a response to the syscall
 					render(text:totalVenueList as JSON,contentType:"application/json")
 				}
 				else{
@@ -349,12 +448,14 @@ class VenueController {
 				}
 			}
 			else{
+				//if apiVersion do not match send errorCode 100
 				response.put("errorCode","100")
 				response.put("errorMessage","API version do not match")
 				render(text:response as JSON,contentType:"application/json")
 			}
 		}
 		catch(Exception e){
+			//if an exception occurs send errorCode 200 along with the exception message
 			log.info("Exception is ===> "+e.getMessage())
 			response.put("errorCode",200)
 			response.put("errorMessage",e.getMessage())
@@ -363,68 +464,97 @@ class VenueController {
 		
 	}
 
-	def startTimer(){
-		def timer = BartsyConfiguration.findByConfigName("timer")
-		boolean flag = timer.value.toBoolean()
-		if(!flag){ //value should be false initially
-			timer.setValue("true")
-			timer.save(flush:true)
+//	def startTimer(){
+//		def timer = BartsyConfiguration.findByConfigName("timer")
+//		boolean flag = timer.value.toBoolean()
+//		if(!flag){ //value should be false initially
+//			timer.setValue("true")
+//			timer.save(flush:true)
+//
+//		}
+//	}
 
-		}
-	}
-
+	/**
+	 * This is the webservice to be called by the bartender app when they receive a heartbeat PN.
+	 *
+	 * @author Swetha Bhatnagar
+	 *
+	 **/
 	def heartBeatVenue = {
+		//defining a map to return as a response for this syscall
 		def response = [:]
 		try{
+			//parse the request sent as input to the syscall
 			def json = JSON.parse(request)
+			//check to make sure the apiVersion sent in the request matches the correct apiVersion
 			def apiVersion = BartsyConfiguration.findByConfigName("apiVersion")
 			if(apiVersion.value.toInteger() == json.apiVersion.toInteger()){
+				//look for a venue based on the venueId sent in the request
 				def venue = Venue.findByVenueId(json.venueId)
 				if(venue){
+					//if venue exists update the column lastHBResponse for venue object to current date time and if venue is in OFFLINE state move it to OPEN state
 					venue.setLastHBResponse(new Date())
 					if(venue.status.equals("OFFLINE"))
 					{
 						venue.status =  "OPEN"
 					}
+					//save the venue object with updated lastHBResponse column value
 					venue.save(flush:true)
+					//send errorCode 0 as confirmation of request has been received
 					response.put("errorCode","0")
 					response.put("errorMessage","Request Received")
-
 				}
 			}
 			else{
+				//if apiVersion do not match send errorCode 100
 				response.put("errorCode","100")
 				response.put("errorMessage","API version do not match")
 			}
 		}
 		catch(Exception e){
+			//if an exception occurs send errorCode 200 along with the exception message
 			log.info("Exception is ===> "+e.getMessage())
 			response.put("errorCode",200)
 			response.put("errorMessage",e.getMessage())
 		}
 		render(text:response as JSON ,  contentType:"application/json")
 	}
-
+	
+	/**
+	 * This is the webservice to be called to change the status of a venue.
+	 *
+	 * @author Swetha Bhatnagar
+	 *
+	 **/
 	def setVenueStatus = {
+		//defining a map to return as a response for this syscall
 		def response = [:]
 		try{
+			//parse the request sent as input to the syscall
 			def json = JSON.parse(request)
+			//check to make sure the apiVersion sent in the request matches the correct apiVersion
 			def apiVersion = BartsyConfiguration.findByConfigName("apiVersion")
 			if(apiVersion.value.toInteger() == json.apiVersion.toInteger()){
+				//look for a venue based on the venueId sent in the request
 				def venue = Venue.findByVenueId(json.venueId)
 				if(venue){
+					//if venue exists set the status of the venue object with the status sent in the request to the syscall
 					venue.setStatus(json.status)
+					//save the venue object
 					venue.save(flush:true)
+					//send the errorCode 0 as acknowledgement of the status save
 					response.put("errorCode","0")
 					response.put("errorMessage","Save Successful")
 				}
 			}
 			else{
+				//if apiVersion do not match send errorCode 100
 				response.put("errorCode","100")
 				response.put("errorMessage","API version do not match")
 			}
 		}
 		catch(Exception e){
+			//if an exception occurs send errorCode 200 along with the exception message
 			log.info("Exception is ===> "+e.getMessage())
 			response.put("errorCode",200)
 			response.put("errorMessage",e.getMessage())
