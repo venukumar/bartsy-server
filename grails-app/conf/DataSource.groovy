0ffbbd574c57e0ad0e1da95e93ac0a93ca354d0e
swethab <swethab@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-06-24 14:09:34 +0000

    replaced create -drop with update
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@208 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -14,7 +14,7 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:mysql://localhost/bartsy"
         }
     }
@@ -26,7 +26,7 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "create-drop"
+            dbCreate = "update"
             url = "jdbc:mysql://localhost/bartsy"
             pooled = true
             properties {
