2e318ad2775489efc6d108527ac031611bb9698f
bhargavi <bhargavi@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-05 14:06:03 +0000

    admin default record
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@270 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,4 +1,5 @@
 import bartsy.BartsyConfiguration
+import bartsy.AdminUser
 import bartsy.IngredientCategory;
 import bartsy.IngredientType;
 
@@ -11,6 +12,10 @@ class BootStrap {
 			new BartsyConfiguration(configName:'userTimeout',value:'30').save(flush:true)
 			new BartsyConfiguration(configName:'venueTimeout',value:'30').save(flush:true)
 			new BartsyConfiguration(configName:'apiVersion',value:'2').save(flush:true)
+		}
+		if(!AdminUser.count()){
+			new AdminUser(userName:'admin',password:'B@rt$y',userType:'Admin').save(flush:true)
+			
 		}
 			  }
     def destroy = {
