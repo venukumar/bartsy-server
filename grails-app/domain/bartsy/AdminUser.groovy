9551738c982e54cb063d3003bdc2d4e4963a25b2
bhargavi <bhargavi@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-05 13:51:42 +0000

    new domain
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@263 c3b44e55-a100-42f6-9c86-32dbf579054b

---
+++ grails-app/domain/bartsy/AdminUser.groovy
@@ -0,0 +1,28 @@
+package bartsy
+
+class AdminUser {
+
+    static constraints = {
+		firstName(nullable:true)
+		lastName(nullable:true)
+		email(nullable:true)
+		username()
+		password()
+		userType(inList: ["Admin","SalesUser"])
+		dateCreated()
+		lastUpdated()
+    }
+	
+	String firstName
+	String lastName
+	String email
+	String username
+	String password
+	String userType
+	Date dateCreated
+	Date lastUpdated
+	
+	static mapping={
+		version false
+	}
+}
