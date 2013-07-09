8b16a479a4bb7085ad670bd986eb6402c1de5e4d
srikantht <srikantht@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-09 13:59:18 +0000

    Added userSessionCode field
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@292 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/domain/bartsy/CheckedInUsers.groovy
+++ grails-app/domain/bartsy/CheckedInUsers.groovy
@@ -4,10 +4,12 @@ class CheckedInUsers {
 
     static constraints = {
 		lastHBResponse(nullable:true)
+		userSessionCode(unique:true)
     }
     
     static belongsTo = [userProfile : UserProfile,venue : Venue]
 	
+	String userSessionCode
 	int status
 	Date dateCreated
 	Date lastUpdated
