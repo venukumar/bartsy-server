33483734a95f4524e7437791bac6601fbc56ed21
bhargavi <bhargavi@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-10 13:30:15 +0000

    updated customer and shipping address information in authorise.net
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@305 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/services/bartsy/PaymentService.groovy
+++ grails-app/services/bartsy/PaymentService.groovy
@@ -4,6 +4,8 @@ package bartsy
 import grails.converters.JSON
 import net.authorize.Merchant
 import net.authorize.data.Order
+import net.authorize.data.Customer
+import net.authorize.data.ShippingAddress
 import net.authorize.data.creditcard.CreditCard
 import net.authorize.Environment
 import net.authorize.aim.Result
@@ -18,6 +20,8 @@ class PaymentService {
 		def response = [:]
 		Merchant merchant = Merchant.createMerchant(Environment.SANDBOX,
 				"75x2yLLj", "5Lq4dG24m63qncQ4");
+			def venueList = CheckedInUsers.findByUserProfile(userprofile)
+			def venueInfo = Venue.get(venueList.venue.id)
 		// create credit card
 		CreditCard creditCard = CreditCard.createCreditCard();
 		creditCard.setCreditCardNumber(userprofile.getCreditCardNumber());
@@ -28,7 +32,27 @@ class PaymentService {
 				TransactionType.AUTH_ONLY, new BigDecimal(price));
 		Order order = new Order()
 		order.setInvoiceNumber(orderId.toString())
-		authCaptureTransaction.setCreditCard(creditCard);
+		
+		Customer customer = Customer.createCustomer()
+		if(userprofile.firstName){
+			customer.setFirstName(userprofile.firstName)
+			customer.setLastName(userprofile.LastName)
+		}else
+			customer.setFirstName(userprofile.nickName)
+		customer.setEmail(userprofile.email)
+		customer.setCustomerId(userprofile.bartsyId)
+		
+		ShippingAddress address = ShippingAddress.createShippingAddress()
+		address.setFirstName(venueInfo.managerName)
+		address.setCompany(venueInfo.venueName)
+		address.setAddress(venueInfo.streetAddress)
+		address.setCity(venueInfo.address)
+		address.setState(venueInfo.region)
+		address.setZipPostalCode(venueInfo.postalCode)
+		
+		authCaptureTransaction.setCreditCard(creditCard)
+		authCaptureTransaction.setCustomer(customer)
+		authCaptureTransaction.setShippingAddress(address)
 		authCaptureTransaction.setOrder(order)
 		Result<Transaction> result = (Result<Transaction>) merchant.postTransaction(authCaptureTransaction);
 		if(result.isApproved()) {
@@ -48,6 +72,9 @@ class PaymentService {
 		def response = [:]
 		Merchant merchant = Merchant.createMerchant(Environment.SANDBOX,
 				"75x2yLLj", "5Lq4dG24m63qncQ4");
+		
+			def venueList = CheckedInUsers.findByUserProfile(userprofile)
+			def venueInfo = Venue.get(venueList.venue.id)
 		// create credit card
 		CreditCard creditCard = CreditCard.createCreditCard();
 		creditCard.setCreditCardNumber(userprofile.getCreditCardNumber());
@@ -58,8 +85,28 @@ class PaymentService {
 				TransactionType.CAPTURE_ONLY, new BigDecimal(price));
 		Order order = new Order()
 		order.setInvoiceNumber(orderId.toString())
+		
+		Customer customer = Customer.createCustomer()
+		if(userprofile.firstName){
+			customer.setFirstName(userprofile.firstName)
+			customer.setLastName(userprofile.LastName)
+		}else
+			customer.setFirstName(userprofile.nickName)
+		customer.setEmail(userprofile.email)
+		customer.setCustomerId(userprofile.bartsyId)
+		
+		ShippingAddress address = ShippingAddress.createShippingAddress()
+		address.setFirstName(venueInfo.managerName)
+		address.setCompany(venueInfo.venueName)
+		address.setAddress(venueInfo.streetAddress)
+		address.setCity(venueInfo.address)
+		address.setState(venueInfo.region)
+		address.setZipPostalCode(venueInfo.postalCode)		
+		
 		authCaptureTransaction.setAuthorizationCode(authCode)
 		authCaptureTransaction.setCreditCard(creditCard);
+		authCaptureTransaction.setCustomer(customer)
+		authCaptureTransaction.setShippingAddress(address)
 		authCaptureTransaction.setOrder(order)
 		Result<Transaction> result = (Result<Transaction>) merchant.postTransaction(authCaptureTransaction);
 		if(result.isApproved()) {
