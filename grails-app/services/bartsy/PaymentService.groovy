e19b6b79e03b641af9a030797abcdc610680c880
srikantht <srikantht@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-11 07:28:49 +0000

    Added new method authorizePaymentInSaveUserProfile
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@306 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/services/bartsy/PaymentService.groovy
+++ grails-app/services/bartsy/PaymentService.groovy
@@ -20,8 +20,8 @@ class PaymentService {
 		def response = [:]
 		Merchant merchant = Merchant.createMerchant(Environment.SANDBOX,
 				"75x2yLLj", "5Lq4dG24m63qncQ4");
-			def venueList = CheckedInUsers.findByUserProfile(userprofile)
-			def venueInfo = Venue.get(venueList.venue.id)
+		def venueList = CheckedInUsers.findByUserProfile(userprofile)
+		def venueInfo = Venue.get(venueList.venue.id)
 		// create credit card
 		CreditCard creditCard = CreditCard.createCreditCard();
 		creditCard.setCreditCardNumber(userprofile.getCreditCardNumber());
@@ -32,16 +32,16 @@ class PaymentService {
 				TransactionType.AUTH_ONLY, new BigDecimal(price));
 		Order order = new Order()
 		order.setInvoiceNumber(orderId.toString())
-		
+
 		Customer customer = Customer.createCustomer()
 		if(userprofile.firstName){
 			customer.setFirstName(userprofile.firstName)
-			customer.setLastName(userprofile.LastName)
+			customer.setLastName(userprofile.lastName)
 		}else
 			customer.setFirstName(userprofile.nickName)
 		customer.setEmail(userprofile.email)
 		customer.setCustomerId(userprofile.bartsyId)
-		
+
 		ShippingAddress address = ShippingAddress.createShippingAddress()
 		address.setFirstName(venueInfo.managerName)
 		address.setCompany(venueInfo.venueName)
@@ -49,7 +49,7 @@ class PaymentService {
 		address.setCity(venueInfo.address)
 		address.setState(venueInfo.region)
 		address.setZipPostalCode(venueInfo.postalCode)
-		
+
 		authCaptureTransaction.setCreditCard(creditCard)
 		authCaptureTransaction.setCustomer(customer)
 		authCaptureTransaction.setShippingAddress(address)
@@ -57,24 +57,53 @@ class PaymentService {
 		Result<Transaction> result = (Result<Transaction>) merchant.postTransaction(authCaptureTransaction);
 		if(result.isApproved()) {
 			response.put("authApproved","true")
-			response.put("authCode",result.getTarget().getAuthorizationCode())	
-			response.put("authTransactionNumber",result.getTarget().getTransactionId())			
+			response.put("authCode",result.getTarget().getAuthorizationCode())
+			response.put("authTransactionNumber",result.getTarget().getTransactionId())
 		}
 		else{
 			response.put("authApproved","false")
 			response.put("authErrorMessage",result.getResponseText())
-		}		
+		}
 		println response
 		return response
 	}
-	
+
+	def authorizePaymentInSaveUserProfile(UserProfile userprofile,def price,def orderId){
+		def response = [:]
+		Merchant merchant = Merchant.createMerchant(Environment.SANDBOX,
+				"75x2yLLj", "5Lq4dG24m63qncQ4");
+		// create credit card
+		CreditCard creditCard = CreditCard.createCreditCard();
+		creditCard.setCreditCardNumber(userprofile.getCreditCardNumber());
+		creditCard.setExpirationMonth(userprofile.getExpMonth());
+		creditCard.setExpirationYear(userprofile.getExpYear());
+		// create transaction
+		Transaction authCaptureTransaction = merchant.createAIMTransaction(
+				TransactionType.AUTH_ONLY, new BigDecimal(price));
+		Order order = new Order()
+		order.setInvoiceNumber(orderId.toString())
+		authCaptureTransaction.setCreditCard(creditCard);
+		authCaptureTransaction.setOrder(order)
+		Result<Transaction> result = (Result<Transaction>) merchant.postTransaction(authCaptureTransaction);
+		if(result.isApproved()) {
+			response.put("authApproved","true")
+			response.put("authCode",result.getTarget().getAuthorizationCode())
+			response.put("authTransactionNumber",result.getTarget().getTransactionId())
+		}
+		else{
+			response.put("authApproved","false")
+			response.put("authErrorMessage",result.getResponseText())
+		}
+		println response
+		return response
+	}
 	def capturePayment(def authCode,UserProfile userprofile,def price,def orderId){
 		def response = [:]
 		Merchant merchant = Merchant.createMerchant(Environment.SANDBOX,
 				"75x2yLLj", "5Lq4dG24m63qncQ4");
-		
-			def venueList = CheckedInUsers.findByUserProfile(userprofile)
-			def venueInfo = Venue.get(venueList.venue.id)
+
+		def venueList = CheckedInUsers.findByUserProfile(userprofile)
+		def venueInfo = Venue.get(venueList.venue.id)
 		// create credit card
 		CreditCard creditCard = CreditCard.createCreditCard();
 		creditCard.setCreditCardNumber(userprofile.getCreditCardNumber());
@@ -85,24 +114,24 @@ class PaymentService {
 				TransactionType.CAPTURE_ONLY, new BigDecimal(price));
 		Order order = new Order()
 		order.setInvoiceNumber(orderId.toString())
-		
+
 		Customer customer = Customer.createCustomer()
 		if(userprofile.firstName){
 			customer.setFirstName(userprofile.firstName)
-			customer.setLastName(userprofile.LastName)
+			customer.setLastName(userprofile.lastName)
 		}else
 			customer.setFirstName(userprofile.nickName)
 		customer.setEmail(userprofile.email)
 		customer.setCustomerId(userprofile.bartsyId)
-		
+
 		ShippingAddress address = ShippingAddress.createShippingAddress()
 		address.setFirstName(venueInfo.managerName)
 		address.setCompany(venueInfo.venueName)
 		address.setAddress(venueInfo.streetAddress)
 		address.setCity(venueInfo.address)
 		address.setState(venueInfo.region)
-		address.setZipPostalCode(venueInfo.postalCode)		
-		
+		address.setZipPostalCode(venueInfo.postalCode)
+
 		authCaptureTransaction.setAuthorizationCode(authCode)
 		authCaptureTransaction.setCreditCard(creditCard);
 		authCaptureTransaction.setCustomer(customer)
@@ -121,10 +150,10 @@ class PaymentService {
 		println response
 		return response
 	}
-	
+
 	def makePayment(order){
 		def captureResponse = capturePayment(order.getAuthCode(),order.user,order.getTotalPrice(),order.getOrderId())
-		if(captureResponse.get("captureApproved").toBoolean()){			
+		if(captureResponse.get("captureApproved").toBoolean()){
 			order.setCaptureApproved("true")
 			order.setCaptureTransactionNumber(captureResponse.get("captureTransactionNumber"))
 		}
