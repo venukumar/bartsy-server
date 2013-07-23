e86d40bc3a61de9c1143456b3407275b53b0abcf
srikantht <srikantht@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-23 14:57:55 +0000

    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@371 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -12,6 +12,9 @@ class BootStrap {
 			new BartsyConfiguration(configName:'userTimeout',value:'30').save(flush:true)
 			new BartsyConfiguration(configName:'venueTimeout',value:'30').save(flush:true)
 			new BartsyConfiguration(configName:'apiVersion',value:'3').save(flush:true)
+			new BartsyConfiguration(configName:'payment',value:'Sand Box').save(flush:true)
+			new BartsyConfiguration(configName:'authId',value:'493ZG9zm2jH4').save(flush:true)
+			new BartsyConfiguration(configName:'authPassword',value:'2q7GcGA3h6KD2W2Y').save(flush:true)
 		}
 		if(!AdminUser.count()){
 			new AdminUser(username:'admin',password:'B@rt$y',userType:'Admin').save(flush:true)
