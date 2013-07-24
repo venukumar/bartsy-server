5c5c47d8db9471b378667710b16675f900c47cc4
srikantht <srikantht@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-24 15:15:40 +0000

    Added new fileds and constraints
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@377 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/domain/bartsy/Cocktails.groovy
+++ grails-app/domain/bartsy/Cocktails.groovy
@@ -4,6 +4,21 @@ import java.util.Date;
 
 class Cocktails {
 
+	static constraints = {
+		cocktailId(nullable:true)
+		name(nullable:true)
+		category(nullable:true)
+		alcohol(nullable:true)
+		glass(nullable:true)
+		ingredients(nullable:true)
+		instructions(nullable:true)
+		price(nullable:true)
+		available(nullable:true)
+		description(nullable:true)
+		shopping(nullable:true)
+	}
+	static belongsTo =[venue:Venue]
+
 	long cocktailId
 	String name
 	String category
@@ -15,9 +30,6 @@ class Cocktails {
 	String available
 	Date dateCreated
 	Date lastUpdated
-	
-	static belongsTo =[venue:Venue]
-	
-	static constraints = {
-	}
+	String description
+	String shopping
 }
