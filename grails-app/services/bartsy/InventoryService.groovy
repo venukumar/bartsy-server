5ad5b37b66f5e0d76c1910a5dd974077968f3482
srikantht <srikantht@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-24 13:27:10 +0000

    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@373 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/services/bartsy/InventoryService.groovy
+++ grails-app/services/bartsy/InventoryService.groovy
@@ -12,28 +12,74 @@ class InventoryService {
 		// if venue id exists in db
 		if(venue){
 			// to get all available cocktails from db based on the venue id
-			def cocktail = Cocktails.findAllByVenue(venue)
+			def cocktails = Cocktails.findAllByVenue(venue)
 			// if cocktails exists
-			if(cocktail){
-			// For store the list of cocktails
-				def cocktailsList=[]
-				cocktail.each {
-					def cocktailObject = it
-					def cocktailMap=[:]
-					// added all cocktail details cocktailsMap
-					cocktailMap.put("cocktailId", cocktailObject.cocktailId)
-					cocktailMap.put("name", cocktailObject.name)
-					cocktailMap.put("category", cocktailObject.category)
-					cocktailMap.put("glass", cocktailObject.glass)
-					cocktailMap.put("alcohol", cocktailObject.alcohol)
-					cocktailMap.put("ingredients", cocktailObject.ingredients)
-					cocktailMap.put("instructions", cocktailObject.instructions)
-					cocktailMap.put("price", cocktailObject.price)
-					cocktailMap.put("available", cocktailObject.available)
-					// stored every cocktailsMap into cocktailsList
-					cocktailsList.add(cocktailMap)
+			if(cocktails){
+				def option_group=[]
+				def menus=[]
+				def menusMap=[:]
+				menusMap.put("menu_name","Bar")
+				def sections=[]
+				def sectionsMap=[:]
+				sectionsMap.put("section_name","Mixed Drinks")
+				def contents=[]
+				cocktails.each {
+					def cocktail=it
+					def contentsMap=[:]
+					def ingredients=[]
+					def photos=[]
+					def options=[]
+					contentsMap=getCocktailMap(cocktail)
+					contentsMap.put("photos",photos)
+					if(cocktail.shopping && cocktail.shopping.length()>0){
+						def categoryList = cocktail.shopping.trim().split(",")
+						if(categoryList){
+							categoryList.each {
+								def categoryId = it
+								def category=IngredientCategory.findById(categoryId)
+								if(category){
+									String name =category.category
+									if(!option_group.contains(categoryId))
+										option_group.add(categoryId)
+									if(!options.contains(name))
+										options.add("option_"+name)
+								}
+							}
+						}
 					}
-				output.put("cocktails", cocktailsList)
+					contentsMap.put("option_groups",options)
+					contents.add(contentsMap)
+				}
+				sectionsMap.put("contents",contents)
+				if(option_group && option_group.size()>0){
+					option_group.each {
+						def categoryId = it
+						def category=IngredientCategory.findById(categoryId)
+						def ing = Ingredients.findAllByVenueAndCategory(venue,category)
+						if(ing){
+							String key="option_"+category.category
+							def categoryIngredients=[]
+							def detailsOfIngList=[]
+							def detailsOfIng=[:]
+							detailsOfIng.put("name",category.category)
+							ing.each {
+								def ingObj=it
+								def option_group_map=[:]
+								option_group_map.put("ingredientId",ingObj.ingredientId)
+								option_group_map.put("name",ingObj.name)
+								option_group_map.put("price",ingObj.price)
+								option_group_map.put("available",ingObj.available)
+								categoryIngredients.add(option_group_map)
+							}
+							detailsOfIng.put("options", categoryIngredients)
+							detailsOfIngList.add(detailsOfIng)
+							sectionsMap.put(key,detailsOfIngList)
+						}
+					}
+				}
+				sections.add(sectionsMap)
+				menusMap.put("sections",sections)
+				output.put("menus", menusMap)
 				output.put("errorCode", 0)
 				output.put("errorMessage", "Cocktails are available")
 				return output
@@ -42,12 +88,29 @@ class InventoryService {
 				// if cocktails doesn't exists
 				return returnNegativeResponse(output,"Cocktails are not available")
 			}
-
 		}
 		else{
 			// If venue id doesn't exist in db, just return the errror message to request
 			return returnNegativeResponse(output,"Venue ID does not exist")
 		}
+	}
+
+	def getCocktailMap(Cocktails cocktail){
+		def contentsMap=[:]
+		contentsMap.put("id",cocktail.id)
+		contentsMap.put("cocktailId",cocktail.cocktailId)
+		contentsMap.put("name", cocktail.name)
+		contentsMap.put("category",cocktail.category)
+		contentsMap.put("alcohol",cocktail.alcohol)
+		contentsMap.put("price", cocktail.price)
+		contentsMap.put("type", "")
+		contentsMap.put("name", cocktail.name)
+		contentsMap.put("glass", cocktail.glass)
+		contentsMap.put("ingredients", cocktail.ingredients)
+		contentsMap.put("description", cocktail.description)
+		contentsMap.put("instructions", cocktail.instructions)
+		contentsMap.put("available", cocktail.available)
+		return contentsMap
 	}
 	/**
 	 * @methodName : This method we are used to delete the ingredient from the DB.
@@ -70,19 +133,19 @@ class InventoryService {
 			// checking ingredient id exists in DB
 			if(ingredient){
 				output.put("ingredientId", ingredientId)
-			def iingredient=Ingredients.get(ingredientId)
-			def status = iingredient.delete()
-			println "statussssss "+status
-			output.put("status", status)
+				def iingredient=Ingredients.get(ingredientId)
+				def status = iingredient.delete()
+				println "statussssss "+status
+				output.put("status", status)
 			}else{
-			// If venue id doesn't exist in db, just return the errror message to client
-			return returnNegativeResponse(output,"Ingredient ID does not exist")
+				// If venue id doesn't exist in db, just return the errror message to client
+				return returnNegativeResponse(output,"Ingredient ID does not exist")
 			}
 		}else{
 			// If venue id doesn't exist in db, just return the errror message to client
 			return returnNegativeResponse(output,"Vneue ID does not exist")
 		}
-		
+
 	}
 	/**
 	 * @methodName : returnNegativeResponse
@@ -95,5 +158,5 @@ class InventoryService {
 		return response
 	}
 
-	
+
 }
