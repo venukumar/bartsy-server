a92e12ca93346d86bfcd883765651d0c90d347f5
srikantht <srikantht@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-26 05:19:50 +0000

    Added getLocuMenuHeaders service
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@391 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/services/bartsy/VenueService.groovy
+++ grails-app/services/bartsy/VenueService.groovy
@@ -4,7 +4,7 @@ import grails.converters.JSON
 
 class VenueService {
 
-	
+
 	/**
 	 * 
 	 * @param json
@@ -15,19 +15,15 @@ class VenueService {
 		try{
 			Venue venue =checkForVenue(json)
 			if(venue){
-				
-				println "Menu "
 				def menu = JSON.parse(URLDecoder.decode(venue.menu))
 				//output.put("menu", menu)
-			
-				
-			    output.put("parsedData",menu)
-				
-				
-//				def specialHours = json.has("specialHours")?json.specialHours.toString().trim():""
-//				def menuHeaders = json.has("menuHeaders")?json.menuHeaders:""
-				
-				
+
+
+				output.put("parsedData",menu)
+
+				//				def specialHours = json.has("specialHours")?json.specialHours.toString().trim():""
+				//				def menuHeaders = json.has("menuHeaders")?json.menuHeaders:""
+
 			}else{
 				output.put("errorCode", 3)
 				output.put("errorMessage","Venue does not exists")
@@ -46,8 +42,33 @@ class VenueService {
 	 */
 	def getLocuMenuHeaders(json,Map output) {
 		try{
-			def venueId =checkForVenue(json)
-			if(venueId){
+			def venue=Venue.findByVenueId(json.venueId)
+			if(venue){
+				if(venue.locuMenu){
+					def locuMenu = JSON.parse(URLDecoder.decode(venue.locuMenu))
+					if(locuMenu.menus){
+						def menus = locuMenu.menus
+						if(menus){
+							def menueHeaders=[]
+							menus.each {
+								def menueObj = it
+								menueHeaders.add(menueObj.menu_name)
+							}
+							output.put("menuHeaders",menueHeaders)
+							output.put("errorCode",0)
+							output.put("currentTime",new Date().toGMTString())
+						}else{
+							output.put("errorCode", 6)
+							output.put("errorMessage","Menue headers not available")
+						}
+					}else{
+						output.put("errorCode", 5)
+						output.put("errorMessage","Locu Menue not available")
+					}
+				}else{
+					output.put("errorCode", 4)
+					output.put("errorMessage","Locu Data not available")
+				}
 			}else{
 				output.put("errorCode", 3)
 				output.put("errorMessage","Venue does not exists")
