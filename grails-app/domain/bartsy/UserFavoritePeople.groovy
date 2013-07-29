97aed1ed35227a3589d3f648a335f29aac50371f
srikantht <srikantht@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-29 09:54:08 +0000

    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@400 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/domain/bartsy/UserFavoritePeople.groovy
+++ grails-app/domain/bartsy/UserFavoritePeople.groovy
@@ -2,14 +2,15 @@ package bartsy
 
 class UserFavoritePeople {
 
-	String userBartsyId
-	String favoriteBartsyId
-	String staus//Like or Unlike
-
 	static constraints = {
 		staus(nullable:false)
 		userBartsyId(nullable:false)
 		favoriteBartsyId(nullable:false)
 	}
-	static belongsTo = [userProfile : UserProfile,venue : Venue]
+	static belongsTo = [user : UserProfile,venue : Venue]
+
+	String userBartsyId
+	String favoriteBartsyId
+	String staus//Like or Unlike
+
 }
