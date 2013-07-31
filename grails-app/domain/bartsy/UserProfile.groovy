c3afc3eb9214876b7af59d5c138e5144d1d8d3b8
srikantht <srikantht@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-31 15:20:53 +0000

    Added new field redactedCardNumber
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@408 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/domain/bartsy/UserProfile.groovy
+++ grails-app/domain/bartsy/UserProfile.groovy
@@ -2,55 +2,57 @@ package bartsy
 
 class UserProfile {
 
-     static constraints = {
-		 emailVerified(nullable:true)
-		 sessionCode(nullable:true)
-		 encryptedCreditCard(nullable:true)
-    }
-	
+	static constraints = {
+		emailVerified(nullable:true)
+		sessionCode(nullable:true)
+		encryptedCreditCard(nullable:true)
+		redactedCardNumber(nullable:true)
+	}
+
 	static mapping = {
 		// userImage(sqlType:'blob')
 	}
-   // static belongsTo = [bartsyUserDetails:BartsyUserDetails]
-	
-        String name
-        String firstName
-		String lastName
-		String dateOfBirth
-		String description
-		String orientation
-		String status
-		String nickName
-		String bartsyPassword
+	// static belongsTo = [bartsyUserDetails:BartsyUserDetails]
+
+	String name
+	String firstName
+	String lastName
+	String dateOfBirth
+	String description
+	String orientation
+	String status
+	String nickName
+	String bartsyPassword
 	//String location
 	//String emailAddress
 	//String mobileNumber
 	//String preference
-        //String gender
-        //String relationshipStatus
-        //String userStatus
-       // String language1
-        //String language2
-        String userImage
-		String googleId
-		String googleUserName
-		String email
-		String facebookId
-		String bartsyId
-		String facebookUserName
-		String gender
-		int deviceType
-		String deviceToken
-		Date dateCreated
-		Date lastUpdated
-		String showProfile
-		Date showProfileUpdated
-		String bartsyLogin
-		String creditCardNumber
-		String expMonth
-		String expYear
-		String emailVerified
-		String sessionCode
-		String encryptedCreditCard
-		int adminUser
-      }
+	//String gender
+	//String relationshipStatus
+	//String userStatus
+	// String language1
+	//String language2
+	String userImage
+	String googleId
+	String googleUserName
+	String email
+	String facebookId
+	String bartsyId
+	String facebookUserName
+	String gender
+	int deviceType
+	String deviceToken
+	Date dateCreated
+	Date lastUpdated
+	String showProfile
+	Date showProfileUpdated
+	String bartsyLogin
+	String creditCardNumber
+	String expMonth
+	String expYear
+	String emailVerified
+	String sessionCode
+	String encryptedCreditCard
+	int adminUser
+	String redactedCardNumber
+}
