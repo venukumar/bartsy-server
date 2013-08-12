a94107967c836d55125b737cae617edae0645628
srikantht <srikantht@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-08-12 10:32:01 +0000

    usr favorites table structure changed
    git-svn-id: http://192.168.2.90/svn/bartsy-server/bartsy-server@466 c3b44e55-a100-42f6-9c86-32dbf579054b

--- grails-app/domain/bartsy/UserFavoriteDrinks.groovy
+++ grails-app/domain/bartsy/UserFavoriteDrinks.groovy
@@ -4,24 +4,33 @@ class UserFavoriteDrinks {
 
 	static constraints = {
 		itemsList()
-		specialInstructions(nullable:true)
+		itemName(nullable:true)
 		description(nullable:true)
 		categorys(nullable:true)
 		title(nullable:true)
 		basePrice(nullable:true)
 		quantity(nullable:true)
+		type(nullable:true)
+		name(nullable:true)
+		order_price(nullable:true)
+		selectedItems(nullable:true)
 	}
 
 
 	static mapping = { itemsList type: 'text' }
 
 	static belongsTo = [user:UserProfile,venue:Venue]
+
 	String title
+	String itemName
 	String itemsList
-	String specialInstructions
+	String name
 	String description
 	String categorys
 	String basePrice
 	String quantity
 	Date dateCreated
+	String type
+	String order_price
+	String selectedItems
 }
